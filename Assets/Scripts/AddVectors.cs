a9155e06685f26c37e88179e6fb20c7091bcf10b
TangChun0807 <tangchun@sheridancollege.ca>
2026-09-11 21:37:17 -0400

    redo the magnitude and the visualizing vector addition exercise.

--- Assets/Scripts/AddVectors.cs
+++ Assets/Scripts/AddVectors.cs
@@ -9,13 +9,38 @@ public class AddVectors : MonoBehaviour
     {
         // Exercise: Visualizing Vector Addition
         // Create a new Vector2 called "rPlusB" which adds rTransform's position to bTransform's position.​
+        Vector2 rPlusB = redTransform.position + blueTransform.position;
 
         // When the B key is held down, draw a blue line from the origin to bTransform's position.​
         // When the R key is held down, draw a red line from the origin to rTransform's position.​
         // When the R and B keys are both held down, draw a magenta from the origin to rPlusB.​
 
+        if (Input.GetKey(KeyCode.B))
+        {
+            Debug.DrawLine(Vector2.zero, blueTransform.position, Color.blue);
+        }
+
+        if (Input.GetKey(KeyCode.R))
+        {
+            Debug.DrawLine(Vector2.zero, redTransform.position, Color.red);
+        }
+
+        if (Input.GetKey(KeyCode.R) && Input.GetKey(KeyCode.B))
+        {
+            Debug.DrawLine(Vector2.zero, rPlusB, Color.magenta);
+        }
+
+
         // Exercise: Calculating Magnitude
         // Calculate the magnitude of bPlusR using the mathematical formula for calculating the length of a vector – Pythagorean Theroem.​
         // Output the magnitude to the console using either Debug.Log or print.
+
+        float x = rPlusB.x;
+        float y = rPlusB.y;
+        float xSqr = x * x;
+        float ySqr = y * y;
+        float sum = xSqr + ySqr;
+        float magnitude = Mathf.Sqrt(sum);
+        Debug.Log(magnitude);
     }
 }
