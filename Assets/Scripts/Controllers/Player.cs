9960a74dbe3bceb272e736dbbfc0cb9f3ac99318
TangChun0807 <tangchun@sheridancollege.ca>
2026-09-21 20:30:55 -0400

    complete task 1 A and B

--- Assets/Scripts/Controllers/Player.cs
+++ Assets/Scripts/Controllers/Player.cs
@@ -8,13 +8,17 @@ public class Player : MonoBehaviour
     public Transform enemyTransform;
     public GameObject bombPrefab;
     public List<Transform> asteroidTransforms;
-    public Vector2 bombOffSet;
+    public Vector2 bombOffset;
+    public float bombTrailSpacing;
+    public int numberOfTrailBomb;
 
     void Start()
     {
         Debug.Log(NormalizeVector(new Vector2(3, 4)));
         Debug.Log(NormalizeVector(new Vector2(-3, 2)));
         Debug.Log(NormalizeVector(new Vector2(1.5f, -3.5f)));
+
+        bombOffset = new Vector2(1, 1);
     }
 
     // Update is called once per frame
@@ -22,7 +26,12 @@ public class Player : MonoBehaviour
     {
         if (Keyboard.current.bKey.wasPressedThisFrame)
         {
-            SpawnBombAtOffset(bombOffSet);
+            SpawnBombAtOffset(bombOffset);
+        }
+
+        if (Keyboard.current.tKey.wasPressedThisFrame)
+        {
+            spawnTrailBomnb(bombTrailSpacing, numberOfTrailBomb);
         }
 
     }
@@ -41,5 +50,25 @@ public class Player : MonoBehaviour
         return outVector;
     }
 
+    void spawnTrailBomnb(float bombTrailSpacing, int numberOfTrailBomb)
+    {
+
+        Vector2 trailBombOffset = new Vector2();
+
+            for (int i = 1; i <= numberOfTrailBomb; i++)
+            {
+                trailBombOffset =  Vector2.down * bombTrailSpacing * i;
+
+                SpawnBombAtOffset(trailBombOffset);
+            }
+        
+    }
+
+    void spawnBombOnRandomCorner(float distance)
+    {
+        int randomCornerPosition = UnityEngine.Random.Range(0, 4);
+
+    }
+    
 }
 
